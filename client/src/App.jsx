571c1624418bd4b7e51c2b8a594bfd79b6861bf7
shannonh0208 <shannon1.holmes@famu.edu>
2026-03-18 22:02:31 -0400

    Add Analytics with visitors, page views, bounce rate, etc.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import "./App.css";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   const [resume, setResume] = useState("");
