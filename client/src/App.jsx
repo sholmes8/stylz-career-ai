7b9109e3da6d64e705c39c9d67c3a391ac1c43bd
Shannon Holmes <77421722+sholmes8@users.noreply.github.com>
2026-03-28 19:28:10 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -204,7 +204,6 @@ const handleDownload = () => {
 
                 <pre>{result.optimizedResume}</pre>
               </div>
-              <Analytics />
             </>
           )}
         </div>
